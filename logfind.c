f6424feb6f77bfd91e876f19aa03ff3e7a7280a7
Aaron <aaronvan@icloud.com>
2020-09-23 16:21:23 -0700

    split files

--- logfind.c
+++ logfind.c
@@ -9,13 +9,15 @@ extern char **global_argv;
 
 char line[COUNT];
 
+void readLine(FILE *fp) {
+    while (fscanf(fp, "%1023s", line) == 1) {
+		compareWords(global_argv[1], line);
+    }
+}
+
 void compareWords(char *a[], char *b[]) {
     int rc = strncmp(global_argv[1], line, COUNT);
     word_compare(rc == 0);
 }
 
-void readLine(FILE *fp) {
-    while (fscanf(fp, "%1023s", line) == 1) {
-	compareWords(global_argv[1], line);
-    }
-}
+
--- main.c
+++ main.c
@@ -9,20 +9,18 @@ char **global_argv;
 
 int main(int argc, char *argv[]) {
 	global_argv = argv[1];
-	int i = 0;
-    
+	
 	if (argc < 2) {
 		puts("You must enter a search word.");
 		goto error;
     }
-	
-    FILE *fp = NULL;
-	    
+		    
     // open the file.
-    fp = fopen("data.txt", "r");
+    FILE *fp = fopen("data.txt", "r");
     check(fp != NULL, "File failed to open.");
 	
-    for (; i < argc; i++) {
+    // iterate by line
+	for (int i = 1; i < argc; i++) {
 		readLine(fp);	
 		rewind(fp);
     }	
